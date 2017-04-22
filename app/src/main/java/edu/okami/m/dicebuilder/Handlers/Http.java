ac4f152e3aff73836a8363eb018985b8c07d4f8c
M <mjdev3030@gmail.com>
2017-04-22 12:38:43 -0400

    HTTPS protocol

---
+++ app/src/main/java/edu/okami/m/dicebuilder/Handlers/Http.java
@@ -0,0 +1,8 @@
+package edu.okami.m.dicebuilder.Handlers;
+
+/**
+ * Created by M on 4/19/2017.
+ */
+
+public class Http {
+}
---
+++ app/src/main/java/edu/okami/m/dicebuilder/Objects/ChatMessage.java
@@ -0,0 +1,8 @@
+package edu.okami.m.dicebuilder.Objects;
+
+/**
+ * Created by M on 4/20/2017.
+ */
+
+public class ChatMessage {
+}
