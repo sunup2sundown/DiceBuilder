7740b8d13e7834ed32bacfe204660fa297bb49bc
Rigel Deming <rigelxrigel@gmail.com>
2017-04-27 00:07:28 -0400

    back to dicebox working and populate dicebox

--- app/src/main/java/edu/okami/m/dicebuilder/DiceBoxActivity.java
+++ app/src/main/java/edu/okami/m/dicebuilder/DiceBoxActivity.java
@@ -1,6 +1,7 @@
 package edu.okami.m.dicebuilder;
 
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -47,12 +48,11 @@ public class DiceBoxActivity extends AppCompatActivity
         Intent i = getIntent();
         boxName = i.getStringExtra("BoxName");
         userId = i.getStringExtra("UserID");
-        String file = userId + "\\" + boxName;
-        Log.d(TAG, file);
 
+        ContextWrapper cw = new ContextWrapper(getApplicationContext());
+        File directory = cw.getDir(userId, getApplicationContext().MODE_PRIVATE);
 
-        boxDirectory = new File(getFilesDir(),file);
-        boxDirectory = getDir(file, Context.MODE_PRIVATE);
+        boxDirectory = new File(directory, boxName);
 
         if(!boxDirectory.exists()){
             boxDirectory.mkdirs();
@@ -125,6 +125,7 @@ public class DiceBoxActivity extends AppCompatActivity
         //gridView.setAdapter(new GridAdapter(this, R.layout.gridview_layout, populateGridWithDice()));
         Intent i = new Intent(getApplicationContext(), textureActivity.class);
         i.putExtra("BoxName", boxName);
+        i.putExtra("UserID", userId);
         startActivity(i);
     }
 
